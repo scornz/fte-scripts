495e1c2e8379b998995cfdd7fdd89e6a1f3115ac
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2025-02-09 21:28:23 -0500

    fix: convert googlemail.com to gmail.com

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -17,5 +17,9 @@ def process_email(email: str):
     # Remove all '.' characters from the local part
     local_part_without_periods = local_part.replace(".", "")
 
+    # If domain part is googlemail.com in any case, change it to gmail.com
+    if domain_part.lower() == "googlemail.com":
+        domain_part = "gmail.com"
+
     # Reconstruct and return the modified email
     return f"{local_part_without_periods}@{domain_part}".lower().strip()
